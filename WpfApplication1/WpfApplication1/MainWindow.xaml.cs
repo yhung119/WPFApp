ce046c99a870140ea39aec77d220fc6983776e56
Daniel Huang <danielhuang37@outlook.com>
2016-06-17 10:48:15 +0800

    first commit after change

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //huehue
         public MainWindow()
         {
             InitializeComponent();
