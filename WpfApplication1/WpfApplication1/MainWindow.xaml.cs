9eae35c95b778ff8ab1135eb6956e999968281a1
Daniel Huang <danielhuang37@outlook.com>
2016-06-21 10:19:11 +0800

    fix

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -12,18 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace WpfApplication1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow 
     {
-        //huehue
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
